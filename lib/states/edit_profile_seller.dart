100f66e68d5838efae6dfbc485c88a0654e8e79f
plamsbb1412 <uiosf1998@gmail.com>
2022-01-06 13:30:54 +0700

    6/1/2022

--- lib/states/edit_profile_seller.dart
+++ lib/states/edit_profile_seller.dart
@@ -1,10 +1,17 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/user_model.dart';
 import 'package:flutter_application_1/utility/my_constant.dart';
+import 'package:flutter_application_1/utility/my_dialog.dart';
+import 'package:flutter_application_1/widgets/show_image.dart';
+import 'package:flutter_application_1/widgets/show_progress.dart';
 import 'package:flutter_application_1/widgets/show_title.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class EditProfileStore extends StatefulWidget {
@@ -23,6 +30,9 @@ class _EditProfileStoreState extends State<EditProfileStore> {
   TextEditingController detailsController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+  File? file;
 
   @override
   void initState() {
@@ -60,22 +70,193 @@ class _EditProfileStoreState extends State<EditProfileStore> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Edit Profile Store'),
+          actions: [
+            IconButton(
+              onPressed: () => processEditProfileSeller(),
+              icon: Icon(Icons.edit),
+            ),
+          ],
         ),
         body: LayoutBuilder(
-          builder: (context, constraints) => ListView(
-            padding: EdgeInsets.all(16),
-            children: [
-              buildTitle('รูปโปรไฟล์  :'),
-              buildTitle('ข้อมูลทั่วไป :'),
-              buildName(constraints),
-              buildfirstName(constraints),
-              buildlastName(constraints),
-              buildDetails(constraints),
-              buildemail(constraints),
-              buildPhone(constraints),
-            ],
+          builder: (context, constraints) => GestureDetector(
+            onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+            behavior: HitTestBehavior.opaque,
+            child: Form(
+              key: formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16),
+                children: [
+                  buildTitle('รูปโปรไฟล์  :'),
+                  buildAvatar(constraints),
+                  buildTitle('ข้อมูลทั่วไป :'),
+                  buildName(constraints),
+                  buildfirstName(constraints),
+                  buildlastName(constraints),
+                  buildDetails(constraints),
+                  buildemail(constraints),
+                  buildPhone(constraints),
+                  buildTitle('รูปหน้าร้าน  :'),
+                  buildAvatar(constraints),
+                  buildButtonEditProfile()
+                ],
+              ),
+            ),
           ),
         ));
+  }
+
+  Future<Null> processEditProfileSeller() async {
+    print('processEditProfileSeller work');
+    MyDialog().showProgressDialog(context);
+
+    if (formKey.currentState!.validate()) {
+      if (file == null) {
+        print('## User Current Avatar');
+        editValueToMySQL(userModel!.avater);
+      } else {
+        String apiSaveAvatar =
+            '${MyConstant.domain}/shoppingmall/saveAvatar.php';
+
+        List<String> nameAvatars = userModel!.avater.split('/');
+        String nameFile = nameAvatars[nameAvatars.length - 1];
+        nameFile = 'edit${Random().nextInt(100)}$nameFile';
+
+        print('## User New Avatar nameFile ==>>> $nameFile');
+
+        Map<String, dynamic> map = {};
+        map['file'] =
+            await MultipartFile.fromFile(file!.path, filename: nameFile);
+        FormData formData = FormData.fromMap(map);
+        await Dio().post(apiSaveAvatar, data: formData).then((value) {
+          print('Upload Succes');
+          String pathAvatar = '/shoppingmall/avatar/$nameFile';
+          editValueToMySQL(pathAvatar);
+        });
+      }
+    }
+  }
+
+  Future<Null> editValueToMySQL(String pathAvatar) async {
+    print('## pathAvatar ==> $pathAvatar');
+    String apiEditProfile =
+        '${MyConstant.domain}/Project/StoreRMUTL/AIP/editProfileSellerWhereid.php?isAdd=true&id=${userModel!.id}&name=${userModel!.name}&firstName=${userModel!.firstName}&lastName=${userModel!.lastName}&name_store=${userModel!.name_store}&details=${userModel!.details}&email=${userModel!.email}&phone=${userModel!.phone}&avater=${userModel!.avater}&profile_store=${userModel!.profile_store}';
+    await Dio().get(apiEditProfile).then((value) {
+      Navigator.pop(context);
+      Navigator.pop(context);
+    });
+  }
+
+  ElevatedButton buildButtonEditProfile() {
+    return ElevatedButton.icon(
+      onPressed: () => processEditProfileSeller(),
+      icon: Icon(Icons.edit),
+      label: Text('แก้ไขข้อมูล'),
+    );
+  }
+
+  Future<Null> createAvatar({ImageSource? source}) async {
+    try {
+      var result = await ImagePicker().getImage(
+        source: source!,
+        maxWidth: 800,
+        maxHeight: 800,
+      );
+      setState(() {
+        file = File(result!.path);
+      });
+    } catch (e) {}
+  }
+
+  Row buildAvatar(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () => createAvatar(source: ImageSource.camera),
+                  icon: Icon(
+                    Icons.add_a_photo,
+                    color: MyConstant.dark,
+                  )),
+              Container(
+                width: constraints.maxWidth * 0.6,
+                height: constraints.maxWidth * 0.6,
+                child: userModel == null
+                    ? ShowProgress()
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: userModel!.avater == null
+                            ? ShowImage(path: MyConstant.imageavatar)
+                            : file == null
+                                ? buildShowImageNetwork()
+                                : Image.file(file!),
+                      ),
+              ),
+              IconButton(
+                  onPressed: () => createAvatar(source: ImageSource.gallery),
+                  icon: Icon(
+                    Icons.add_photo_alternate,
+                    color: MyConstant.dark,
+                  )),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  CachedNetworkImage buildShowImageNetwork() {
+    return CachedNetworkImage(
+      imageUrl:
+          '${MyConstant.domain}/Project/StoreRMUTL/AIP${userModel!.avater}',
+      placeholder: (context, url) => ShowProgress(),
+    );
+  }
+
+  Row buildProfile(BoxConstraints constraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_a_photo,
+                    color: MyConstant.dark,
+                  )),
+              Container(
+                width: constraints.maxWidth * 0.6,
+                height: constraints.maxWidth * 0.6,
+                child: userModel == null
+                    ? ShowProgress()
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: userModel!.avater == null
+                            ? ShowImage(path: MyConstant.imageavatar)
+                            : CachedNetworkImage(
+                                imageUrl:
+                                    '${MyConstant.domain}/Project/StoreRMUTL/AIP${userModel!.profile_store}',
+                                placeholder: (context, url) => ShowProgress(),
+                              ),
+                      ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_photo_alternate,
+                    color: MyConstant.dark,
+                  )),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   Row buildName(BoxConstraints constraints) {
@@ -86,6 +267,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Name';
+              } else {
+                return null;
+              }
+            },
             controller: nameController,
             decoration: InputDecoration(
               labelText: 'ชื่อ :',
@@ -105,6 +293,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill firstName';
+              } else {
+                return null;
+              }
+            },
             controller: firstNameController,
             decoration: InputDecoration(
               labelText: 'ชื่อจริง :',
@@ -124,6 +319,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill lastName';
+              } else {
+                return null;
+              }
+            },
             controller: lastNameController,
             decoration: InputDecoration(
               labelText: 'นามสกุล :',
@@ -143,6 +345,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill NameStore';
+              } else {
+                return null;
+              }
+            },
             controller: nameStoreController,
             decoration: InputDecoration(
               labelText: 'ชื่อร้าน :',
@@ -162,6 +371,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Details';
+              } else {
+                return null;
+              }
+            },
             maxLines: 3,
             controller: detailsController,
             decoration: InputDecoration(
@@ -182,6 +398,13 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill email';
+              } else {
+                return null;
+              }
+            },
             controller: emailController,
             decoration: InputDecoration(
               labelText: 'Email :',
@@ -201,6 +424,14 @@ class _EditProfileStoreState extends State<EditProfileStore> {
           margin: EdgeInsets.only(top: 16),
           width: constraints.maxWidth * 0.6,
           child: TextFormField(
+            keyboardType: TextInputType.phone,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Phone';
+              } else {
+                return null;
+              }
+            },
             controller: phoneController,
             decoration: InputDecoration(
               labelText: 'เบอร์โทร :',
