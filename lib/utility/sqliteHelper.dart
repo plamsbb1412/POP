adc698f68b878d5d5fbfa8012b8d6378b079b847
plamsbb1412 <uiosf1998@gmail.com>
2022-01-25 14:21:35 +0700

    25/ๅ

--- lib/utility/sqliteHelper.dart
+++ lib/utility/sqliteHelper.dart
@@ -36,11 +36,31 @@ class SQLiterHelper {
     Database database = await connectedDatabase();
     List<SQLiteModel> results = [];
     List<Map<String, dynamic>> maps = await database.query(tableDatabase);
-    print('### maps on SQLiteHelper ==>> $maps');
+    // print('### maps on SQLitHelper ==>> $maps');
     for (var item in maps) {
       SQLiteModel model = SQLiteModel.fromMap(item);
       results.add(model);
     }
     return results;
+  }
+
+  Future<Null> insertValueToSQLite(SQLiteModel sqLiteModel) async {
+    Database database = await connectedDatabase();
+    await database.insert(tableDatabase, sqLiteModel.toMap()).then(
+        (value) => print('### insert Value name ==>> ${sqLiteModel.name}'));
+  }
+
+  Future<void> deleteSQLiteWhereId(int id) async {
+    Database database = await connectedDatabase();
+    await database
+        .delete(tableDatabase, where: '$columnId = $id')
+        .then((value) => print('### Success Delete id ==> $id'));
+  }
+
+  Future<void> emptySQLite() async {
+    Database database = await connectedDatabase();
+    await database
+        .delete(tableDatabase)
+        .then((value) => print('### Empty SQLite Success'));
   }
 }
